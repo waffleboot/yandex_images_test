079de94fde3aa86f092e3428fc0e22748fdd1389
Andrei Yangabishev <waffleboot@gmail.com>
2018-03-22 00:42:39 +0300

    add CellView and add table view to ViewController

---
+++ yandex_images_test/CellView.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class CellView: UITableViewCell {
+
+    @IBOutlet private var siteImage: UIImageView?
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+
+}
--- yandex_images_test/ViewController.swift
+++ yandex_images_test/ViewController.swift
@@ -1,19 +1,41 @@
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    private struct Item {
+        var date: String
+        var name: String
+    }
+
+    private var data = [Item]()
+    
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction private func addImage() {
+        data.append(Item(date: "date", name: "name"))
+        tableView.reloadData()
     }
-
+    
+    @IBAction private func clear() {
+        data = [Item]()
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = data[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellView", for: indexPath) as! CellView
+        cell.dateLabel.text = item.date
+        cell.nameLabel.text = item.name
+        return cell
+    }
 
 }
-
