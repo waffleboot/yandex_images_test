208c3023cd0f0be0950462c4622fbd912f4ffd9c
Andrei Yangabishev <waffleboot@gmail.com>
2018-03-23 22:51:42 +0300

    strange user defaults error

--- yandex_images_test/Models/DataStorage.swift
+++ yandex_images_test/Models/DataStorage.swift
@@ -6,7 +6,10 @@ struct DataStorage {
     private static let lock = NSLock()
 
     static func load() -> [Item] {
-        guard let data = UserDefaults.standard.object(forKey: "items") as? Data else { return [Item]() }
+        guard let data = UserDefaults.standard.object(forKey: "items") as? Data else {
+            print("Strange UserDefaults error");
+            return [Item]()
+        }
         return NSKeyedUnarchiver.unarchiveObject(with: data) as! [Item]
     }
     
